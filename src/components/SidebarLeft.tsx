262cc2ced88ae18718e9a403a0cc544fcd666de5
Edson Gaxiola <edsongaxiola77@gmail.com>
2025-11-06 19:11:24 -0600

    Probando los componentes de cronometro

--- src/components/SidebarLeft.tsx
+++ src/components/SidebarLeft.tsx
@@ -3,7 +3,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import styles from './SidebarLeft.module.css';
-import Crono from "app/components/Cronometro/cronometro";
+import Cronometro from './cronometro';  
 
 export default function SidebarLeft() {
   return (
@@ -19,12 +19,12 @@ export default function SidebarLeft() {
       <div className={styles.userData}>
         <h3>Datos de usuario</h3>
         <div className={styles.statusIndicators}>
-        <div className={`${styles.statusDot} ${styles.dotBlue}`}></div>
-        <div className={`${styles.statusDot} ${styles.dotGreen}`}></div>
-        <div className={`${styles.statusDot} ${styles.dotOrange}`}></div>
-        <div className={`${styles.statusDot} ${styles.dotYellow}`}></div>
-        <div className={`${styles.statusDot} ${styles.dotRed}`}></div>
-      </div>
+          <div className={`${styles.statusDot} ${styles.dotBlue}`}></div>
+          <div className={`${styles.statusDot} ${styles.dotGreen}`}></div>
+          <div className={`${styles.statusDot} ${styles.dotOrange}`}></div>
+          <div className={`${styles.statusDot} ${styles.dotYellow}`}></div>
+          <div className={`${styles.statusDot} ${styles.dotRed}`}></div>
+        </div>
         <p>Sistema operativo</p>
         <Link href="/User/user">
           <button className={styles.btnViewUser}>Ver</button>
@@ -35,21 +35,21 @@ export default function SidebarLeft() {
         <h3>Tiempo en URANUS</h3>
         <div className={styles.timeDisplay}>
           <div className={styles.cronom}> 
-          <Crono/>
-        </div>
+            <Cronometro />  {/* ✅ Úsalo como Cronometro, NO como Crono */}
+          </div>
         </div>
       </div>
 
       <div className={styles.logout}>
         <Link href="/">
-            <Image
-              src="/icons/power-icon.webp"
-              alt="Salir"
-              width={40}
-              height={40}
-              className={styles.logoutButton}
-            />
-          </Link>
+          <Image
+            src="/icons/power-icon.webp"
+            alt="Salir"
+            width={40}
+            height={40}
+            className={styles.logoutButton}
+          />
+        </Link>
       </div>
     </div>
   );
